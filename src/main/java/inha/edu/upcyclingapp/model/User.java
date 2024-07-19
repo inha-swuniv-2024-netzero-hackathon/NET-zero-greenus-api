fadb4c7773d6d934c9a057b7dec20727d5611993
s.sunyoung <ajufresh@gmail.com>
2024-07-19 17:39:05 +0900

    add db connect info

---
+++ src/main/java/inha/edu/upcyclingapp/model/User.java
@@ -0,0 +1,33 @@
+package inha.edu.upcyclingapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @Column(nullable = false, length = 50)
+    private String nickname;
+}
---
+++ src/main/java/inha/edu/upcyclingapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package inha.edu.upcyclingapp.repository;
+
+import inha.edu.upcyclingapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
