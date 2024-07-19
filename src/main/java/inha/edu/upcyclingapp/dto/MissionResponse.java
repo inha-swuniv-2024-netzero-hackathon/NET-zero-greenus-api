72549e649e17132a47440a6ab1160c3c1f9fc896
s.sunyoung <ajufresh@gmail.com>
2024-07-20 06:45:04 +0900

    MissionResponse에 id 추가하게 변경

--- src/main/java/inha/edu/upcyclingapp/dto/MissionResponse.java
+++ src/main/java/inha/edu/upcyclingapp/dto/MissionResponse.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class MissionResponse {
+    private Long id;
     private String title;
     private String description;
     private Boolean isCompleted;
     private String imageUrl;
 
     public MissionResponse(Mission mission, String imageUrl) {
+        this.id = mission.getId();
         this.title = mission.getMissionContent();
         this.description = mission.getDescription();
         this.isCompleted = mission.getIsCompleted();
