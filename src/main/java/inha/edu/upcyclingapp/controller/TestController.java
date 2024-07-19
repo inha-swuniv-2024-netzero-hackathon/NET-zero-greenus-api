08cbe0b944afb44d2c42131533629d7adbd866a3
s.sunyoung <ajufresh@gmail.com>
2024-07-19 16:33:16 +0900

    add health check

--- src/main/java/inha/edu/upcyclingapp/controller/TestController.java
+++ src/main/java/inha/edu/upcyclingapp/controller/TestController.java
@@ -1,13 +1,13 @@
 package inha.edu.upcyclingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class TestController {
 
     @GetMapping("/health")
     public String test() {
-        return "health";
+        return "healthy";
     }
 }
